be365d505f154295d895f2ad66c049d531d3445d
Anand A. Joshi <ajoshi@usc.edu>
2022-02-28 15:19:16 -0800

    update

--- fetal_mri/main_dejitter_mri.m
+++ fetal_mri/main_dejitter_mri.m
@@ -3,11 +3,15 @@ addpath(genpath('/home/ajoshi/projects/svreg/src'));
 addpath(genpath('/home/ajoshi/projects/svreg/3rdParty'));
 addpath(genpath('/home/ajoshi/projects/svreg/MEX_Files'));
 
-v1=load_untouch_nii('/home/ajoshi/Desktop/tmp/haste.nii.gz');
+v1=load_untouch_nii('/home/ajoshi/Downloads/disc_data/fetal_mri/fetal_vol2_t2_haste_trufi/t2_haste_cor_TE119/_t2_haste_cor_TE119_20210915181625_9_bst.nii');%('/home/ajoshi/Desktop/tmp/haste.nii.gz');
 
 v1.img = double(v1.img);
+
+im_sz_x = size(v1.img,2);im_sz_y = size(v1.img,3);
+
+
 vout=v1;
-[Tx,Ty]=meshgrid(1:256);
+[Tx,Ty]=meshgrid(1:im_sz_x,1:im_sz_y);
 
 for j = 1:size(v1.img,1)
     Tx_full{j} = Tx;Ty_full{j}=Ty;
@@ -22,23 +26,24 @@ for dejitter=1:10
         I1 = v1_rescaled([j-1,j+1],:,:); I1=permute(I1,[2,3,1]);
         I2 = squeeze(v1_rescaled(j,:,:));
 
-
+        I1r(:,:,1)=imresize(I1(:,:,1),[256,256]);I1r(:,:,2)=imresize(I1(:,:,2),[256,256]);
+        I2r(:,:,1)=imresize(I2(:,:,1),[256,256]);I2r(:,:,2)=I2r(:,:,1);
         tic;
-        [Iw,Tx,Ty] = demon_image_registration_mri(I1,repmat(I2,[1,1,2]));
+        [Iw,Tx,Ty] = demon_image_registration_mri(I1r,I2r);
         toc;
-
-        Tx_full{j} = interp2(Tx_full{j},Tx,Ty);
-        Ty_full{j} = interp2(Ty_full{j},Tx,Ty);
+        Tx = im_sz_x*Tx/256;Ty = im_sz_y*Ty/256;
+        Tx_full{j} = imresize(interp2(Tx_full{j},Tx,Ty),size(v1.img,2:3));
+        Ty_full{j} = imresize(interp2(Ty_full{j},Tx,Ty),size(v1.img,2:3));
 
         IW=interp2(squeeze(v1.img(j,:,:)),Tx_full{j},Ty_full{j},'cubic');
-        vout.img(j,:,:)=imresize(IW,size(v1.img,2:3));
+        vout.img(j,:,:)=IW;
 
 
         fprintf('=====================%d/%d==%d/%d======================',j,size(v1.img,1),dejitter,10);
         close all;
     end
 
-    save_untouch_nii_gz(vout,sprintf('out_%d.nii.gz',dejitter));
+    save_untouch_nii_gz(vout,sprintf('out2_%d.nii.gz',dejitter));
 
 end
 
--- fetal_mri/main_upsample_mri.m
+++ fetal_mri/main_upsample_mri.m
@@ -3,16 +3,21 @@ addpath(genpath('/home/ajoshi/projects/svreg/src'));
 addpath(genpath('/home/ajoshi/projects/svreg/3rdParty'));
 addpath(genpath('/home/ajoshi/projects/svreg/MEX_Files'));
 
-v1=load_untouch_nii('out_2.nii.gz');
+v1=load_untouch_nii('out2_2.nii.gz');
 
 v1.img = double(v1.img);
 vout=v1;
+
+size_x = size(v1.img,2);
+size_y = size(v1.img,3);
+
+
 vout.hdr.dime.pixdim(2)=vout.hdr.dime.pixdim(2)/2.0;
 vout.hdr.dime.dim(2)=2*size(v1.img,1);
 vout.img=zeros(size(v1.img,1)*2,size(v1.img,2),size(v1.img,3));
 vout.img(2:2:end,:,:)=v1.img;
 save_untouch_nii_gz(vout,'temp.nii.gz');
-[X,Y]=meshgrid(1:256);
+[X,Y]=meshgrid(1:size_x,1:size_y);
 v1_rescaled=255*(v1.img/max(v1.img(:)));
 
 for j = 1:size(v1.img,1)-1
@@ -20,11 +25,16 @@ for j = 1:size(v1.img,1)-1
     I1 = v1_rescaled(j,:,:); I1=permute(I1,[2,3,1]);
     I2 = squeeze(v1_rescaled(j+1,:,:));
 
-
+    I1 = imresize(I1,[256,256]);
+    I2 = imresize(I2,[256,256]);
     [~,~,~,Tx1,Ty1] = demon_image_registration_mri(I1,I2);
     [~,~,~,Tx2,Ty2] = demon_image_registration_mri(I2,I1);
 
-
+    Tx1 = (size_x/256) * imresize(Tx1,[size_x,size_y]);
+    Tx2 = (size_x/256) * imresize(Tx2,[size_x,size_y]);
+    Ty1 = (size_x/256) * imresize(Ty1,[size_x,size_y]);
+    Ty2 = (size_x/256) * imresize(Ty2,[size_x,size_y]);
+    
     IW1=interp2(squeeze(v1.img(j+1,:,:)),X+Tx1/2,Y+Ty1/2,'cubic');
     IW2=interp2(squeeze(v1.img(j,:,:)),X+Tx2/2,Y+Ty2/2,'cubic');
     
@@ -32,6 +42,6 @@ for j = 1:size(v1.img,1)-1
 j
 end
 
-save_untouch_nii_gz(vout,'out_up.nii.gz');
+save_untouch_nii_gz(vout,'out2_up.nii.gz');
 
 
