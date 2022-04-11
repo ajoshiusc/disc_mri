83b48929dc7428f3f8eadba8e515ed78c4ba6621
Anand A. Joshi <ajoshi@usc.edu>
2022-04-11 13:53:11 -0700

    update

--- superres_mri/make_data.py
+++ superres_mri/make_data.py
@@ -6,7 +6,8 @@ import h5py
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
-
+from skimage.measure import block_reduce
+from skimage.transform import resize
 mode = 'train'
 
 num_samp = 30000
@@ -15,44 +16,54 @@ patch_size = [256, 256]
 X = list()
 Y = list()
 
-
-im1 = np.random.randn(patch_size[0],patch_size[1])
-im2 = np.random.randn(patch_size[0],patch_size[1])
-im3 = np.random.randn(patch_size[0],patch_size[1])
-im4 = np.random.randn(patch_size[0],patch_size[1])
-im5 = np.random.randn(patch_size[0],patch_size[1])
-
-
-
-im1 = gaussian_filter(im1,1)
-im2 = gaussian_filter(im2,2)*2
-im3 = gaussian_filter(im3,3)*3
-im4 = gaussian_filter(im4,4)*4
-im5 = gaussian_filter(im5,5)*5
-
-
-
-im = np.maximum(im1,im2)
-im = np.maximum(im,im3)
-im = np.maximum(im,im4)
-im = np.maximum(im,im5)
-
-im = im/np.max(im)
-
-plt.imshow(im,cmap='gray')
-plt.colorbar()
-
-plt.show()
-
-
-
 for sub1 in tqdm(range(num_samp)):
 
-        X.append(np.uint8(255.0*slice/slice.max()))
-        Y.append(np.uint8(labels))
+
+        im1 = np.random.rand(patch_size[0],patch_size[1])
+        im2 = np.random.rand(patch_size[0],patch_size[1])
+        im3 = np.random.rand(patch_size[0],patch_size[1])
+        im4 = np.random.rand(patch_size[0],patch_size[1])
+        im5 = np.random.rand(patch_size[0],patch_size[1])
 
 
-hf = h5py.File('/deneb_disk/headreco_out/'+mode+'.h5', 'w')
+
+        #im1 = gaussian_filter(im1,1)
+        im1 = im1-np.min(im1)
+        im1 = im1/np.max(im1)
+        im1 = np.minimum(im1,.5)/.5
+
+        im2 = gaussian_filter(im2,2)
+        im2 = im2-np.min(im2)
+        im2 = im2/np.max(im2)
+        im2 = np.minimum(im2,.8)/.8
+
+        
+        im = np.maximum(im1,im2)
+
+        im = im/np.max(im)
+
+        im_orig = im.copy()
+
+        imsize = im.shape
+        im = block_reduce(im,block_size=(4,1))
+
+        im = resize(im,imsize)
+
+        '''plt.imshow(im,cmap='gray')
+        plt.colorbar()
+        plt.show()
+
+        plt.imshow(im_orig,cmap='gray')
+        plt.colorbar()
+        plt.show()'''
+
+
+
+        X.append(np.uint8(255.0*im/im.max()))
+        Y.append(np.uint8(im_orig))
+
+
+hf = h5py.File('sim_noise_'+mode+'.h5', 'w')
 hf.create_dataset('X', data=X)
 hf.create_dataset('Y', data=Y)
 hf.close()
