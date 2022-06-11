eec2b1258d76823e69d4329ee1f0cf91082f55b3
Anand A. Joshi <ajoshi@usc.edu>
2022-06-10 20:41:55 -0700

    update

--- main_convert_dicomms_to_nifti.py
+++ main_convert_dicomms_to_nifti.py
@@ -1,3 +1,31 @@
 #||AUM||
 #||Shree Ganeshaya Namaha||
 
+import dicom2nifti
+import glob
+import os
+
+scans_dir = '/ImagePTE1/ajoshi/data/fetal_scan_june_8_2022/Vol368_Fetal/Pediatric_Fetal - 1/'
+
+
+scans = glob.glob(scans_dir+"/*")
+out_dir = '/home/ajoshi/fetal_scan_6_8_2022'
+
+
+for s in scans:
+    print(s)
+
+    dicom2nifti.convert_dir.convert_directory(s, out_dir, compression=True)
+
+
+
+
+
+
+
+
+
+
+
+
+
