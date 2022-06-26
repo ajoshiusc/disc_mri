e37db24ebc9005f07c8d1097cfe730044b2a35a9
Anand A. Joshi <ajoshi@usc.edu>
2022-06-26 12:20:55 -0700

    update

--- fetal_mri/main_rigid_reg_stack2atlas.py
+++ fetal_mri/main_rigid_reg_stack2atlas.py
@@ -5,16 +5,18 @@ import numpy as np
 import os
 
 
-sub_img = '/deneb_disk/fetal_scan_6_13_2022/haste_rot_v2/outSVR2.nii.gz'
+input_dir = '/home/ajoshi/mydocker/fetal_scan_maria_demo/fetal_mri_datasets/motion_corrupted_sstse_brain_phantom_stacks/stacks/'
 
-atlas_img = '/home/ajoshi/projects/disc_mri/fetal_mri/fetal_atlas/CRL_FetalBrainAtlas_2017v3/STA31.nii.gz'
+atlas_img = '/home/ajoshi/mydocker/fetal_scan_maria_demo/fetal_mri_datasets/motion_corrupted_sstse_brain_phantom_stacks/stacks/simulated-stack-d2.nii.gz'
 
-input_dir, sub_vol = os.path.split(sub_img)
+#atlas_img = '/home/ajoshi/projects/disc_mri/fetal_mri/fetal_atlas/CRL_FetalBrainAtlas_2017v3/STA32.nii.gz'
+sub_vol = 'simulated-stack-d1.nii.gz'
+
+sub_img = os.path.join(input_dir,sub_vol)
 
 #atlas_img = sub_img
 
 out_sub_img = os.path.join(input_dir,'atlas_'+sub_vol)
-out_sub_img2 = os.path.join(input_dir,'atlas2_'+sub_vol)
 
 fixed_image =  sitk.ReadImage(atlas_img, sitk.sitkFloat32)
 moving_image = sitk.ReadImage(sub_img, sitk.sitkFloat32)
@@ -31,29 +33,27 @@ registration_method = sitk.ImageRegistrationMethod()
 # Similarity metric settings.
 registration_method.SetMetricAsMattesMutualInformation(numberOfHistogramBins=50)
 registration_method.SetMetricSamplingStrategy(registration_method.RANDOM)
-registration_method.SetMetricSamplingPercentage(.5)
+registration_method.SetMetricSamplingPercentage(1)
 
 registration_method.SetInterpolator(sitk.sitkNearestNeighbor)
 
 # Optimizer settings.
 registration_method.SetOptimizerAsGradientDescent(learningRate=1, numberOfIterations=1000, convergenceMinimumValue=1e-6, convergenceWindowSize=10)
-registration_method.SetOptimizerScalesFromPhysicalShift()
+#registration_method.SetOptimizerScalesFromPhysicalShift()
 
 # Setup for the multi-resolution framework.            
-registration_method.SetShrinkFactorsPerLevel(shrinkFactors = [4,2,1])
-registration_method.SetSmoothingSigmasPerLevel(smoothingSigmas=[2,1,0])
-registration_method.SmoothingSigmasAreSpecifiedInPhysicalUnitsOn()
+#registration_method.SetShrinkFactorsPerLevel(shrinkFactors = [4,2,1])
+#registration_method.SetSmoothingSigmasPerLevel(smoothingSigmas=[2,1,0])
+#registration_method.SmoothingSigmasAreSpecifiedInPhysicalUnitsOn()
 
 
 # angle_z = -pi, 0, pi
-#registration_method.SetOptimizerAsExhaustive(numberOfSteps=[3,3,3,3,3,3], stepLength = np.pi)
-#registration_method.SetOptimizerScales([1,1,1,1,1,1])
+registration_method.SetOptimizerAsExhaustive(numberOfSteps=[0,0,0,3,3,3], stepLength = 5*np.pi)
+registration_method.SetOptimizerScales([1,1,1,1,1,1])
 
 
 # Don't optimize in-place, we would possibly like to run this cell multiple times.
-optimized_transform = sitk.Euler3DTransform()    
-registration_method.SetMovingInitialTransform(initial_transform)
-registration_method.SetInitialTransform(optimized_transform, inPlace=False)
+registration_method.SetInitialTransform(initial_transform, inPlace=False)
 
 # Connect all of the observers so that we can perform plotting during registration.
 #registration_method.AddCommand(sitk.sitkStartEvent, rgui.start_plot)
