e080090e1ec66832d31ec3c28ed9328b0b1f3371
Anand A. Joshi <ajoshi@usc.edu>
2023-01-02 15:04:05 -0800

    update

---
+++ fetal_mri/main_growth_chart_ventricle_vol.py
@@ -0,0 +1,32 @@
+import numpy as np
+import glob
+from tqdm import tqdm
+import SimpleITK as sitk
+import xml.etree.ElementTree as ET
+import pandas as pd
+from nilearn.image import load_img
+from pandas import read_csv
+import os
+
+
+lst_files = glob.glob('/deneb_disk/feta_2022/feta_2.2/sub-*/anat/s*dseg.nii.gz')
+
+meta_data = read_csv('/deneb_disk/feta_2022/feta_2.2/participants.tsv',sep='\t',index_col=0)
+
+vent_vols = np.zeros(len(lst_files))
+healthy = np.zeros(len(lst_files))
+
+for i, fname in enumerate(lst_files):
+
+    sub_id = os.path.basename(fname)[:7]
+    healthy[i] = meta_data['Pathology'][sub_id]
+
+    v = load_img(fname) 
+    vox_vol = np.prod(v.header['pixdim'][1:4])
+
+    vent_vols[i] = np.sum(v.get_fdata() == vent_roi_id)*vox_vol
+
+
+
+
+
--- main_convert_dicomms_to_nifti.py
+++ main_convert_dicomms_to_nifti.py
@@ -5,11 +5,11 @@ import glob
 import os
 
 #scans_dir = '/deneb_disk/fetal_scan_8_3_2022/data'
-scans_dir = '/deneb_disk/low_field_pns_device_fmri/11_16_2022/VOL504_PNS_22_11_16-15_46_28-STD-1_3_12_2_1107_5_2_18_41185/SOPHIA_LIBRARY_20221116_160143_945000'
-scans = glob.glob(scans_dir+"/E*")
+scans_dir = '/deneb_disk/fetal_scan_12_14_2022'
+scans = glob.glob(scans_dir+"/v*")
 
 #out_dir = '/deneb_disk/fetal_scan_8_3_2022/nifti'
-out_dir = '/deneb_disk/low_field_pns_device_fmri/11_16_2022/nii_files'
+out_dir = '/deneb_disk/fetal_scan_12_14_2022/nii_files'
 
 if not os.path.isdir(out_dir):
     os.mkdir(out_dir)
