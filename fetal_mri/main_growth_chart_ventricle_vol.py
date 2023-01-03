8a9b04f1f93e674693a49b17d4fad38a279d444b
Anand A. Joshi <ajoshi@usc.edu>
2023-01-02 21:45:48 -0800

    update

--- fetal_mri/main_growth_chart_ventricle_vol.py
+++ fetal_mri/main_growth_chart_ventricle_vol.py
@@ -15,12 +15,13 @@ meta_data = read_csv('/deneb_disk/feta_2022/feta_2.2/participants.tsv',sep='\t',
 
 vent_vols = np.zeros(len(lst_files))
 healthy = np.zeros(len(lst_files))
+g_age = np.zeros(len(lst_files))
 
 for i, fname in enumerate(lst_files):
 
     sub_id = os.path.basename(fname)[:7]
     healthy[i] = meta_data['Pathology'][sub_id]
-
+    g_age[i] = meta_data['Gestational age'][sub_id]
     v = load_img(fname) 
     vox_vol = np.prod(v.header['pixdim'][1:4])
 
