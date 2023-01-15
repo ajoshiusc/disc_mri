c8d883548455bc6a0c4f795cbd514f6eb5d4c16c
Anand A. Joshi <ajoshi@usc.edu>
2023-01-15 15:09:04 -0800

    update

--- fetal_mri/main_svr_process.py
+++ fetal_mri/main_svr_process.py
@@ -4,7 +4,12 @@ import numpy as np
 import os
 import SimpleITK as sitk
 import glob
-
+from monai.metrics.regression import SSIMMetric, MSEMetric
+from nilearn.image import load_img
+from monai.transforms import EnsureChannelFirst
+from tqdm import tqdm
+from monai.losses.ssim_loss import SSIMLoss
+import matplotlib.pyplot as plt
 
 subdir = '/deneb_disk/fetal_scan_1_9_2023/morning/nii_files_rot'
 template = subdir + '/p28_t2_haste_sag_head_p.nii.gz'
@@ -18,8 +23,7 @@ res = 1
 th = 3
 
 
-
-for num_stacks in range(len(stacks)+1):
+for num_stacks in range(1, len(stacks)+1):
 
     str_stacks = ''
     str_th = ''
@@ -28,9 +32,11 @@ for num_stacks in range(len(stacks)+1):
         str_stacks += ' ' + s
         str_th += ' ' + str(th)
 
-
     outsvr = subdir + '/outsvr'+'_'+str(num_stacks)+'.nii.gz'
     outsvr_aligned = subdir + '/outsvr'+'_'+str(num_stacks)+'_aligned.nii.gz'
+
+    if os.path.isfile(outsvr_aligned):
+        continue
 
     cmd = 'mirtksvr reconstruct ' + outsvr + ' ' + \
         str(num_stacks) + str_stacks + ' --resolution ' + str(res)
@@ -40,10 +46,35 @@ for num_stacks in range(len(stacks)+1):
     print(cmd)
     os.system(cmd)
 
-    cmd = 'flirt -in ' + outsvr + ' -ref /home/ajoshi/projects/disc_mri/fetal_mri/fetal_atlas/CRL_FetalBrainAtlas_2017v3/STA31.nii.gz -out '+outsvr_aligned+' -dof 7 -omat reorient.mat -searchrx -180 180 -searchry -180 180 -searchrz -180 180'
-
+    cmd = 'flirt -in ' + outsvr + ' -ref /home/ajoshi/projects/disc_mri/fetal_mri/fetal_atlas/CRL_FetalBrainAtlas_2017v3/STA31.nii.gz -out ' + \
+        outsvr_aligned+' -dof 7 -omat reorient.mat -searchrx -180 180 -searchry -180 180 -searchrz -180 180'
 
     print(cmd)
     os.system(cmd)
 
 
+val_ssim = np.zeros(len(stacks))
+val_mse = np.zeros(len(stacks))
+
+for num_stacks in tqdm(range(1, len(stacks)+1)):
+
+    outsvr_aligned = subdir + '/outsvr'+'_'+str(num_stacks)+'_aligned.nii.gz'
+    target = subdir + '/outsvr'+'_'+str(len(stacks))+'_aligned.nii.gz'
+
+    x = load_img(outsvr_aligned).get_fdata()
+    y = load_img(target).get_fdata()
+
+    x = EnsureChannelFirst(channel_dim=1)(x[None,None,])
+    y = EnsureChannelFirst(channel_dim=1)(y[None,None,])
+
+    data_range = y.max().unsqueeze(0)
+    m = SSIMLoss(spatial_dims=3)
+    val_ssim[num_stacks-1] = m.forward(x,y,data_range=data_range)
+
+print(val_ssim)
+
+x = range(1, len(stacks)+1)
+
+plt.plot(x[2:],val_ssim[2:])
+
+plt.savefig('ssim.png')
