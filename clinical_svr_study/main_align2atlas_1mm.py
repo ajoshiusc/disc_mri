683b45eef11f0e63dc5e0c61b9bd2e758209796c
Anand A. Joshi <ajoshi@usc.edu>
2023-03-04 00:55:04 -0800

    update

---
+++ clinical_svr_study/main_align2atlas_1mm.py
@@ -0,0 +1,26 @@
+import nilearn.image as ni
+import numpy as np
+import os
+import glob
+
+
+import matplotlib.pyplot as plt
+
+from nilearn.image import resample_img
+
+subdirs = glob.glob('/home/ajoshi/projects/disc_mri/clinical_svr_study/SVR_3D_NIFTI/S*.nii.gz')
+atlas = '/home/ajoshi/projects/disc_mri/fetal_mri/fetal_atlas/CRL_FetalBrainAtlas_2017v3/STA25.nii.gz'
+
+atlas_1mm = resample_img(atlas,target_affine=np.eye(3))
+atlas_1mm.set_data_dtype(np.int16)
+atlas_1mm.to_filename('atlas_1mm.nii.gz')
+atlas_1mm = 'atlas_1mm.nii.gz'
+
+for sub in subdirs:
+
+    subname = os.path.basename(sub)
+    sub_aligned = subname[:-7]+'_aligned.nii.gz'
+    cmd = 'flirt -in ' + sub + ' -ref '+ atlas_1mm +' -out ' + sub_aligned+' -dof 6 -omat reorient.mat -searchrx -180 180 -searchry -180 180 -searchrz -180 180'
+
+    print(cmd)
+    os.system(cmd)
