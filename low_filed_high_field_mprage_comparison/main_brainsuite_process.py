a9aee19109d442443cbb90a0c1215591619b7786
Anand A. Joshi <ajoshi@usc.edu>
2023-03-14 22:03:23 -0700

    update

--- low_filed_high_field_mprage_comparison/main_brainsuite_process.py
+++ low_filed_high_field_mprage_comparison/main_brainsuite_process.py
@@ -1,5 +1,37 @@
-||AUM||
+import os
+import glob
+import nibabel as nib
+import numpy as np
 
-||Shree Ganeshaya Namaha||
+
+
+nsub = 5
+sess = 1
+
+for n in range(1,nsub+1):
+    sub_nii = glob.glob('/deneb_disk/low_field_mprage_data/subj' + str(n) + '_vol' + str(sess) + '/*.nii')[0]
+    out_dir = '/deneb_disk/low_field_mprage_data/BrainSuite/subj' + str(n) + '_vol' + str(sess) 
+    
+    if not os.path.isdir(out_dir):
+        os.makedirs(out_dir)
+    
+    out_file = out_dir + '/T1.nii.gz'
+
+    img = nib.load(sub_nii)
+    data = img.get_fdata()
+
+    p = np.percentile(data, 95)
+    print(f' max : {data.max()} and min : {data.min()} and 95 percentile: {p}') # doctest: +SKIP 3237.0
+    new_data = data.copy()
+
+    new_data = np.minimum(200.0*data/p,255)
+    
+
+    new_img = nib.Nifti1Image(new_data, img.affine, img.header)
+
+    new_img.to_filename(out_file)
+    type(new_img)
+
+
 
 
