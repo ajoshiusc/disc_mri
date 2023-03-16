0f3667fe44ded781ec959d8aaabd5fb7c7e7ed69
Anand A. Joshi <ajoshi@usc.edu>
2023-03-15 21:32:06 -0700

    update

--- low_filed_high_field_mprage_comparison/main_brainsuite_process_low_field_SynthSR.py
+++ low_filed_high_field_mprage_comparison/main_brainsuite_process_low_field_SynthSR.py
@@ -9,7 +9,7 @@ nsub = 5
 
 for sess, n in product((1,2), range(1,nsub+1)):
     sub_nii = glob.glob('/deneb_disk/3T_vs_low_field/low_field_mprage_data/subj' + str(n) + '_vol' + str(sess) + '/*.nii')[0]
-    out_dir = '/deneb_disk/3T_vs_low_field/low_field_mprage_data_BrainSuite/subj' + str(n) + '_vol' + str(sess) 
+    out_dir = '/deneb_disk/3T_vs_low_field/low_field_mprage_data_BrainSuite_SynthSR/subj' + str(n) + '_vol' + str(sess) 
     
     if not os.path.isdir(out_dir):
         os.makedirs(out_dir)
@@ -25,12 +25,14 @@ for sess, n in product((1,2), range(1,nsub+1)):
 
     new_data = np.minimum(200.0*data/p,255)
     
-
     new_img = nib.Nifti1Image(new_data, img.affine, img.header,dtype=np.uint16)
-
     new_img.to_filename(out_file)
 
-    cmd = '/home/ajoshi/BrainSuite21a/bin/brainsuite_anatomical_pipeline.sh ' + out_file
+    out_file_synth = out_dir + '/T1Synth.nii.gz'
+    cmd = f'./synthsr.sh {out_file} {out_file_synth}'
+    os.system(cmd)
+
+    cmd = '/home/ajoshi/BrainSuite21a/bin/brainsuite_anatomical_pipeline.sh ' + out_file_synth
     os.system(cmd)
 
 """     out_file = out_dir + '/T1.bse.nii.gz'
