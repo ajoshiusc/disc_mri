6b5acba6da07add675d6f86da7ba5b37d4b7cd45
Anand A. Joshi <ajoshi@usc.edu>
2023-03-15 20:54:47 -0700

    update

--- low_filed_high_field_mprage_comparison/main_brainsuite_process_3T.py
+++ low_filed_high_field_mprage_comparison/main_brainsuite_process_3T.py
@@ -3,12 +3,12 @@ import glob
 import nibabel as nib
 import numpy as np
 import SimpleITK as sitk
+from itertools import product
 
 
 nsub = 5
-sess = 1
 
-for n in range(1,nsub+1):
+for sess, n in product((1,2), range(1,nsub+1)):
     sub_nii = glob.glob('/deneb_disk/3T_vs_low_field/3T_mprage_data/subj' + str(n) + '_vol' + str(sess) + '/*.nii')[0]
     out_dir = '/deneb_disk/3T_vs_low_field/3T_mprage_data_BrainSuite/subj' + str(n) + '_vol' + str(sess) 
     
@@ -30,6 +30,10 @@ for n in range(1,nsub+1):
     new_img = nib.Nifti1Image(new_data, img.affine, img.header,dtype=np.uint16)
 
     new_img.to_filename(out_file)
+
+    cmd = '/home/ajoshi/BrainSuite21a/bin/brainsuite_anatomical_pipeline.sh ' + out_file
+    os.system(cmd)
+
 """ 
     out_file = out_dir + '/T1.bse.nii.gz'
 
--- low_filed_high_field_mprage_comparison/main_brainsuite_process_low_field.py
+++ low_filed_high_field_mprage_comparison/main_brainsuite_process_low_field.py
@@ -2,13 +2,13 @@ import os
 import glob
 import nibabel as nib
 import numpy as np
-
+import SimpleITK as sitk
 
 
 nsub = 5
 sess = 1
 
-for n in range(1,nsub+1):
+for n in range(3,nsub+1):
     sub_nii = glob.glob('/deneb_disk/low_field_mprage_data/subj' + str(n) + '_vol' + str(sess) + '/*.nii')[0]
     out_dir = '/deneb_disk/low_field_mprage_data/BrainSuite/subj' + str(n) + '_vol' + str(sess) 
     
@@ -20,17 +20,28 @@ for n in range(1,nsub+1):
     img = nib.load(sub_nii)
     data = img.get_fdata()
 
-    p = np.percentile(data, 95)
-    print(f' max : {data.max()} and min : {data.min()} and 95 percentile: {p}') # doctest: +SKIP 3237.0
+    p = np.percentile(data, 99.999)
+    print(f' max : {data.max()} and min : {data.min()} and 98 percentile: {p}') # doctest: +SKIP 3237.0
     new_data = data.copy()
 
     new_data = np.minimum(200.0*data/p,255)
     
 
-    new_img = nib.Nifti1Image(new_data, img.affine, img.header)
+    new_img = nib.Nifti1Image(new_data, img.affine, img.header,dtype=np.uint16)
 
     new_img.to_filename(out_file)
-    type(new_img)
+
+    out_file = out_dir + '/T1.bse.nii.gz'
+
+    input = sitk.ReadImage(out_file)
+    image = sitk.Cast(input, sitk.sitkFloat32)
+    # print(type(image))
+    correctedImg = sitk.N4BiasFieldCorrection(image)
+
+    out_file = out_dir + '/T1_N4.nii.gz'
+
+    sitk.WriteImage(correctedImg, out_file) 
+
 
 
 
