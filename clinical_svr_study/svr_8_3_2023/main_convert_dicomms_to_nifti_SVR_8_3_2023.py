c2da6c368619b88baa1f2972a7ffd10523c4b856
Anand A. Joshi <ajoshi@usc.edu>
2023-08-03 22:55:10 -0700

    update

--- clinical_svr_study/svr_8_3_2023/main_convert_dicomms_to_nifti_SVR_8_3_2023.py
+++ clinical_svr_study/svr_8_3_2023/main_convert_dicomms_to_nifti_SVR_8_3_2023.py
@@ -4,13 +4,14 @@ import dicom2nifti
 import glob
 import os
 from zipfile import ZipFile
-#scans_dir = '/deneb_disk/fetal_scan_8_3_2022/data'
-scans_dir = '/deneb_disk/SVR_8_3_2023/SVR11-30'
-sub_data = glob.glob(scans_dir+"/*.zip")
 
-#out_dir = '/deneb_disk/fetal_scan_8_3_2022/nifti'
-out_dir = '/deneb_disk/SVR_8_3_2023/unzipped_dicomms'
-nii_dir = '/deneb_disk/SVR_8_3_2023/nii_data'
+# scans_dir = '/deneb_disk/fetal_scan_8_3_2022/data'
+scans_dir = "/deneb_disk/SVR_8_3_2023/SVR11-30"
+sub_data = glob.glob(scans_dir + "/*.zip")
+
+# out_dir = '/deneb_disk/fetal_scan_8_3_2022/nifti'
+out_dir = "/deneb_disk/SVR_8_3_2023/unzipped_dicomms"
+nii_dir = "/deneb_disk/SVR_8_3_2023/nii_data"
 
 if not os.path.isdir(out_dir):
     os.mkdir(out_dir)
@@ -20,45 +21,27 @@ if not os.path.isdir(nii_dir):
 
 
 for s in sub_data:
-
     dir, fname = os.path.split(s)
-    out_sub_dir = out_dir + '/' + fname[:-4]
+    out_sub_dir = out_dir + "/" + fname[:-4]
 
     if not os.path.isdir(out_sub_dir):
         os.mkdir(out_sub_dir)
 
-    with ZipFile(s,"r") as zip_ref:
+    with ZipFile(s, "r") as zip_ref:
         zip_ref.extractall(out_sub_dir)
-    
 
-    d = glob.glob(out_sub_dir+'/*')
-    scans = glob.glob(d[0]+'/*')
+    d = glob.glob(out_sub_dir + "/S*")
+    scans = glob.glob(d[0] + "/*")
 
-    if not os.path.isdir(scans_dir + '/sub_nii'):
-        os.mkdir(scans_dir + '/sub_nii')
-    
     for s in scans:
         print(s)
 
-        out_sub_nii_dir = nii_dir + '/' + fname[:-4]
+        out_sub_nii_dir = nii_dir + "/" + fname[:-4]
 
         if not os.path.isdir(out_sub_nii_dir):
             os.mkdir(out_sub_nii_dir)
 
-        #dicom2nifti.convert_dir.convert_directory(s, out_sub_nii_dir)
+        # dicom2nifti.convert_dir.convert_directory(s, out_sub_nii_dir)
 
-        cmd = f'dcm2niix -z y -o \'{out_sub_nii_dir}\' \'{s}\''
+        cmd = f"dcm2niix -z y -o '{out_sub_nii_dir}' '{s}'"
         os.system(cmd)
-
-
-
-
-
-
-
-
-
-
-
-
-
