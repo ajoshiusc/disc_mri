4e81d8e9e7ab1d2b14fb557537cb1fc0d22470e3
Anand A. Joshi <ajoshi@usc.edu>
2023-09-26 14:51:32 -0700

    update

--- low_field_high_field_mprage_comparison/main_brainsuite_process_3T.py
+++ low_field_high_field_mprage_comparison/main_brainsuite_process_3T.py
@@ -21,63 +21,71 @@ from multiprocessing import Pool
 
  """
 
-def regparfun(cmd_str):
 
+def regparfun(cmd_str):
     os.system(cmd_str)
-    
+
     return True
 
 
-
 def main():
-
-
     nsub = 5
 
     cmds_all = list()
 
-    for sess, n in product((1,2), range(1,nsub+1)):
-        sub_nii = glob.glob('/deneb_disk/3T_vs_low_field/3T_mprage_data/subj' + str(n) + '_vol' + str(sess) + '/*.nii')[0]
-        out_dir = '/deneb_disk/3T_vs_low_field/3T_mprage_data_BrainSuite/subj' + str(n) + '_vol' + str(sess) 
-        
+    for sess, n in product((1, 2), range(1, nsub + 1)):
+        sub_nii = glob.glob(
+            "/deneb_disk/3T_vs_low_field/3T_mprage_data/subj"
+            + str(n)
+            + "_vol"
+            + str(sess)
+            + "/*.nii"
+        )[0]
+        out_dir = (
+            "/deneb_disk/3T_vs_low_field/3T_mprage_data_BrainSuite/subj"
+            + str(n)
+            + "_vol"
+            + str(sess)
+        )
+
         if not os.path.isdir(out_dir):
             os.makedirs(out_dir)
-        
-        out_file = out_dir + '/T1.nii.gz'
+
+        out_file = out_dir + "/T1.nii.gz"
 
         img = nib.load(sub_nii)
         data = img.get_fdata()
 
         p = np.percentile(data, 98)
-        print(f' max : {data.max()} and min : {data.min()} and 98 percentile: {p}') # doctest: +SKIP 3237.0
+        print(
+            f" max : {data.max()} and min : {data.min()} and 98 percentile: {p}"
+        )  # doctest: +SKIP 3237.0
         new_data = data.copy()
 
-        new_data = np.minimum(200.0*data/p,255)
-        
+        new_data = np.minimum(200.0 * data / p, 255)
 
-        new_img = nib.Nifti1Image(new_data, img.affine, img.header,dtype=np.uint16)
+        new_img = nib.Nifti1Image(new_data, img.affine, img.header, dtype=np.uint16)
 
         new_img.to_filename(out_file)
 
-        cmd = '/home/ajoshi/BrainSuite23a/bin/brainsuite_anatomical_pipeline.sh ' + out_file
-        #os.system(cmd)
+        cmd = (
+            "/home/ajoshi/BrainSuite23a/bin/brainsuite_anatomical_pipeline.sh "
+            + out_file
+        )
+        # os.system(cmd)
         cmds_all.append(cmd)
-    
+
     pool = Pool(processes=4)
 
     #    regparfun(subsnotdone[2])
 
-    print('++++++++++++++')
+    print("++++++++++++++")
     pool.map(regparfun, cmds_all)
-    print('++++SUBMITTED++++++')
+    print("++++SUBMITTED++++++")
 
     pool.close()
     pool.join()
 
 
-
-
 if __name__ == "__main__":
     main()
-
-
--- low_field_high_field_mprage_comparison/main_brainsuite_process_low_field.py
+++ low_field_high_field_mprage_comparison/main_brainsuite_process_low_field.py
@@ -5,33 +5,86 @@ import numpy as np
 import SimpleITK as sitk
 from itertools import product
 
-nsub = 5
+from multiprocessing import Pool
 
-for sess, n in product((1,2), range(1,nsub+1)):
-    sub_nii = glob.glob('/deneb_disk/3T_vs_low_field/low_field_mprage_data/subj' + str(n) + '_vol' + str(sess) + '/*.nii')[0]
-    out_dir = '/deneb_disk/3T_vs_low_field/low_field_mprage_data_BrainSuite/subj' + str(n) + '_vol' + str(sess) 
-    
-    if not os.path.isdir(out_dir):
-        os.makedirs(out_dir)
-    
-    out_file = out_dir + '/T1.nii.gz'
 
-    img = nib.load(sub_nii)
-    data = img.get_fdata()
 
-    p = np.percentile(data, 98)
-    print(f' max : {data.max()} and min : {data.min()} and 98 percentile: {p}') # doctest: +SKIP 3237.0
-    new_data = data.copy()
+def regparfun(cmd_str):
+    os.system(cmd_str)
 
-    new_data = np.minimum(200.0*data/p,255)
-    
+    return True
 
-    new_img = nib.Nifti1Image(new_data, img.affine, img.header,dtype=np.uint16)
 
-    new_img.to_filename(out_file)
+def main():
+    nsub = 5
 
-    cmd = '/home/ajoshi/BrainSuite21a/bin/brainsuite_anatomical_pipeline.sh ' + out_file
-    os.system(cmd)
+    cmds_all = list()
+
+    nsub = 5
+    param_list = {"1e-14", "2e-14"}
+
+    for sess, n, param in product((1, 2), range(1, nsub + 1), param_list):
+        sub_nii = glob.glob(
+            "/deneb_disk/3T_vs_low_field/justins_recons/low_field_mprage_data/subj"
+            + str(n)
+            + "_vol"
+            + str(sess)
+            + "/"
+            + param
+            + "/*.nii"
+        )[0]
+        out_dir = (
+            "/deneb_disk/3T_vs_low_field/justins_recons/low_field_mprage_data_BrainSuite/subj"
+            + str(n)
+            + "_vol"
+            + str(sess)
+            + "/"
+            + param
+        )
+
+        if not os.path.isdir(out_dir):
+            os.makedirs(out_dir)
+
+        out_file = out_dir + "/T1.nii.gz"
+
+        img = nib.load(sub_nii)
+        data = img.get_fdata()
+
+        p = np.percentile(data, 98)
+        print(
+            f" max : {data.max()} and min : {data.min()} and 98 percentile: {p}"
+        )  # doctest: +SKIP 3237.0
+        new_data = data.copy()
+
+        new_data = np.minimum(200.0 * data / p, 255)
+
+        new_img = nib.Nifti1Image(new_data, img.affine, img.header, dtype=np.uint16)
+
+        new_img.to_filename(out_file)
+
+        cmd = (
+            "/home/ajoshi/BrainSuite23a/bin/brainsuite_anatomical_pipeline.sh "
+            + out_file
+        )
+        # os.system(cmd)
+
+        cmds_all.append(cmd)
+
+    pool = Pool(processes=4)
+
+    #    regparfun(subsnotdone[2])
+
+    print("++++++++++++++")
+    pool.map(regparfun, cmds_all)
+    print("++++SUBMITTED++++++")
+
+    pool.close()
+    pool.join()
+
+
+if __name__ == "__main__":
+    main()
+
 
 """     out_file = out_dir + '/T1.bse.nii.gz'
 
@@ -45,6 +98,3 @@ for sess, n in product((1,2), range(1,nsub+1)):
     sitk.WriteImage(correctedImg, out_file) 
 
  """
-
-
-
