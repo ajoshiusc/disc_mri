0e93c831103c12777c0c80e90c1997660dd2ff06
Anand A. Joshi <ajoshi@usc.edu>
2023-09-29 01:49:08 -0700

    update

--- low_field_high_field_mprage_comparison/main_plot_roi_vols_stats_brainsuite.py
+++ low_field_high_field_mprage_comparison/main_plot_roi_vols_stats_brainsuite.py
@@ -28,29 +28,54 @@ label_data = label_img.get_fdata()
 
 # Input array of scalars (assuming it has the same dimensions as the label image)
 roi_vols_3t = np.load('brainSuite_3T.npz')['roi_vols']
+roi_vols_lf = np.load('brainSuite_low_field.npz')['roi_vols']
 label_ids = np.load('brainSuite_3T.npz')['label_ids']
 
+# for LF dims of roi_vols_lf is session, subj, param, roino 
+# for 3T dims of roi_vols_3t is session, subj, roino 
 
 stat = np.zeros(label_data.shape)
 
+param=0
 for i, idx in enumerate(label_ids):
-    stat[label_data == idx] = roi_vols_3t[i]
+    if idx==2000 or idx==0:
+        continue
+    stat[label_data == idx] = np.linalg.norm(roi_vols_3t[0,:,i]-roi_vols_3t[1,:,i])
 
-# Create a color-coded plot of ROIs using Nilearn
-plotting.plot_roi(
-    label_img, bg_img=None, cmap="Paired", title="Color-coded ROIs", colorbar=True
-)
-plotting.plot_stat_map(
-    label_img, bg_img=None, cut_coords=None, display_mode="z", alpha=0.7, colorbar=True
-)
 
 # Overlay scalar data on top of the ROIs
 plotting.plot_stat_map(
     nib.Nifti1Image(stat, label_img.affine),
-    bg_img=None,
+    bg_img=nifti_file_path,
     cmap="viridis",
-    title="Scalar Overlay",
-    colorbar=True,
+    title="3T",threshold=0,
+    colorbar=True,vmax=4000,
 )
 
 plt.show()
+
+
+
+
+stat = np.zeros(label_data.shape)
+
+param=0
+for i, idx in enumerate(label_ids):
+    if idx==2000 or idx==0:
+        continue
+    stat[label_data == idx] = np.linalg.norm(roi_vols_lf[0,:,0, i]-roi_vols_lf[1,:,0, i])
+
+
+# Overlay scalar data on top of the ROIs
+plotting.plot_stat_map(
+    nib.Nifti1Image(stat, label_img.affine),
+    bg_img=nifti_file_path,
+    cmap="viridis",
+    title="0.55T",threshold=0,
+    colorbar=True,vmax=4000,
+)
+
+plt.show()
+
+print('done')
+
