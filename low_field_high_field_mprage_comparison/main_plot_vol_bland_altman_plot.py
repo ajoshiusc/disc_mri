fdb98abc5f4cc531340705630dfef068a6386767
Anand A Joshi <ajoshi@usc.edu>
2023-10-06 10:25:22 -0700

    update

--- low_field_high_field_mprage_comparison/main_plot_vol_bland_altman_plot.py
+++ low_field_high_field_mprage_comparison/main_plot_vol_bland_altman_plot.py
@@ -32,8 +32,8 @@ roi_vols_lf = np.load("brainSuite_low_field.npz")["roi_vols"]
 label_ids = np.load("brainSuite_3T.npz")["label_ids"]
 
 
-roi_vols_3t = roi_vols_3t[:,:,1:-1]
-roi_vols_lf = roi_vols_lf[:,:,0,1:-1]
+roi_vols_3t = roi_vols_3t[:, :, 1:-1]
+roi_vols_lf = roi_vols_lf[:, :, 0, 1:-1]
 
 # for LF dims of roi_vols_lf is session, subj, param, roino
 # for 3T dims of roi_vols_3t is session, subj, roino
@@ -41,54 +41,80 @@ roi_vols_lf = roi_vols_lf[:,:,0,1:-1]
 stat_3t_intra = np.zeros(label_data.shape)
 
 param = 0
-#roi_vols_3t = np.mean(roi_vols_3t,axis=0).flatten()
-#roi_vols_lf = np.mean(roi_vols_lf,axis=0).flatten()
+# roi_vols_3t = np.mean(roi_vols_3t,axis=0).flatten()
+# roi_vols_lf = np.mean(roi_vols_lf,axis=0).flatten()
 
-roi_vols_3t = roi_vols_3t.reshape(2,-1)
-roi_vols_lf = roi_vols_lf.reshape(2,-1)
+roi_vols_3t = roi_vols_3t.reshape(2, -1)
+roi_vols_lf = roi_vols_lf.reshape(2, -1)
 
 # Sample data for the 1st and 2nd repetitions at 0.55T and 3T
 data_0_55T = {
-    'First Repetition': roi_vols_lf[0],  # Replace with your data
-    'Second Repetition': roi_vols_lf[1]  # Replace with your data
+    "First Repetition": roi_vols_lf[0],  # Replace with your data
+    "Second Repetition": roi_vols_lf[1],  # Replace with your data
 }
 
 data_3T = {
-    'First Repetition': roi_vols_3t[0],  # Replace with your data
-    'Second Repetition': roi_vols_3t[1] # Replace with your data
+    "First Repetition": roi_vols_3t[0],  # Replace with your data
+    "Second Repetition": roi_vols_3t[1],  # Replace with your data
 }
 
 # Calculate the differences and means for both datasets
-diff_0_55T = np.array(data_0_55T['First Repetition']) - np.array(data_0_55T['Second Repetition'])
-mean_0_55T = (np.array(data_0_55T['First Repetition']) + np.array(data_0_55T['Second Repetition'])) / 2
+diff_0_55T = np.array(data_0_55T["First Repetition"]) - np.array(
+    data_0_55T["Second Repetition"]
+)
+mean_0_55T = (
+    np.array(data_0_55T["First Repetition"]) + np.array(data_0_55T["Second Repetition"])
+) / 2
 
-diff_3T = np.array(data_3T['First Repetition']) - np.array(data_3T['Second Repetition'])
-mean_3T = (np.array(data_3T['First Repetition']) + np.array(data_3T['Second Repetition'])) / 2
+diff_3T = np.array(data_3T["First Repetition"]) - np.array(data_3T["Second Repetition"])
+mean_3T = (
+    np.array(data_3T["First Repetition"]) + np.array(data_3T["Second Repetition"])
+) / 2
 
 # Bland-Altman Plot for 0.55T
 plt.figure(figsize=(8, 6))
-plt.scatter(mean_0_55T, diff_0_55T, c='blue', marker='o', label='0.55T')
-plt.axhline(np.mean(diff_0_55T), color='red', linestyle='--', label='Bias')
-plt.axhline(np.mean(diff_0_55T) + 1.96 * np.std(diff_0_55T), color='gray', linestyle='--', label='Upper LoA')
-plt.axhline(np.mean(diff_0_55T) - 1.96 * np.std(diff_0_55T), color='gray', linestyle='--', label='Lower LoA')
-plt.xlabel('Mean of 1st and 2nd Repetitions')
-plt.ylabel('Difference (1st - 2nd Repetition)')
+plt.scatter(mean_0_55T, diff_0_55T, c="blue", marker="o", label="0.55T")
+plt.axhline(np.mean(diff_0_55T), color="red", linestyle="--", label="Bias")
+plt.axhline(
+    np.mean(diff_0_55T) + 1.96 * np.std(diff_0_55T),
+    color="gray",
+    linestyle="--",
+    label="Upper LoA",
+)
+plt.axhline(
+    np.mean(diff_0_55T) - 1.96 * np.std(diff_0_55T),
+    color="gray",
+    linestyle="--",
+    label="Lower LoA",
+)
+plt.xlabel("Mean of 1st and 2nd Repetitions")
+plt.ylabel("Difference (1st - 2nd Repetition)")
 plt.legend()
-plt.title('Bland-Altman Plot for 0.55T')
+plt.title("Bland-Altman Plot for 0.55T")
 plt.grid(True)
-plt.savefig('Bland-Altman Plot for 0.55T.png')
+plt.savefig("Bland-Altman Plot for 0.55T.png")
 
 # Bland-Altman Plot for 3T
 plt.figure(figsize=(8, 6))
-plt.scatter(mean_3T, diff_3T, c='green', marker='s', label='3T')
-plt.axhline(np.mean(diff_3T), color='red', linestyle='--', label='Bias')
-plt.axhline(np.mean(diff_3T) + 1.96 * np.std(diff_3T), color='gray', linestyle='--', label='Upper LoA')
-plt.axhline(np.mean(diff_3T) - 1.96 * np.std(diff_3T), color='gray', linestyle='--', label='Lower LoA')
-plt.xlabel('Mean of 1st and 2nd Repetitions')
-plt.ylabel('Difference (1st - 2nd Repetition)')
+plt.scatter(mean_3T, diff_3T, c="green", marker="s", label="3T")
+plt.axhline(np.mean(diff_3T), color="red", linestyle="--", label="Bias")
+plt.axhline(
+    np.mean(diff_3T) + 1.96 * np.std(diff_3T),
+    color="gray",
+    linestyle="--",
+    label="Upper LoA",
+)
+plt.axhline(
+    np.mean(diff_3T) - 1.96 * np.std(diff_3T),
+    color="gray",
+    linestyle="--",
+    label="Lower LoA",
+)
+plt.xlabel("Mean of 1st and 2nd Repetitions")
+plt.ylabel("Difference (1st - 2nd Repetition)")
 plt.legend()
-plt.title('Bland-Altman Plot for 3T')
+plt.title("Bland-Altman Plot for 3T")
 plt.grid(True)
-plt.savefig('Bland-Altman Plot for 3T.png')
+plt.savefig("Bland-Altman Plot for 3T.png")
 
 plt.show()
