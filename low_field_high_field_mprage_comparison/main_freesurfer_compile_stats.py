02ceb65cba8470e23f39c4b84a3f8d23c4d1534e
Anand A. Joshi <ajoshi@usc.edu>
2023-12-05 13:24:46 -0800

    update

--- low_field_high_field_mprage_comparison/main_freesurfer_compile_stats.py
+++ low_field_high_field_mprage_comparison/main_freesurfer_compile_stats.py
@@ -1,5 +1,7 @@
 import os
 import glob
+from re import sub
+from turtle import left
 import nibabel as nib
 import numpy as np
 import SimpleITK as sitk
@@ -12,67 +14,70 @@ import numpy as np
 from nilearn.plotting import plot_roi
 from nilearn import surface
 
+FREESURFER_SUB = "/deneb_disk/3T_vs_low_field/freesurfer_processed_subjects"
 
-def get_roiwise_thickness(left_thickness_file, right_thickness_file, left_label_data, right_label_data):
+import matplotlib.pyplot as plt
+#plt.plot(range(10), range(10))
+#plt.show()
+import matplotlib
+matplotlib.use('TkAgg')
 
+def get_roiwise_thickness(
+    left_thickness_file,
+    right_thickness_file,
+    left_label_data,
+    right_label_data,
+    cortical_roi_ids,
+):
     # Load the left fsaverage surface
-    #fsaverage_surf_left = surface.load_surf_mesh(f'{FREESURFER_SUB}/fsaverage/surf/lh.white')
+    # fsaverage_surf_left = surface.load_surf_mesh(f'{FREESURFER_SUB}/fsaverage/surf/lh.white')
 
     # Load the right fsaverage surface
-    #fsaverage_surf_right = surface.load_surf_mesh(f'{FREESURFER_SUB}/fsaverage/surf/rh.white')
+    # fsaverage_surf_right = surface.load_surf_mesh(f'{FREESURFER_SUB}/fsaverage/surf/rh.white')
 
     # Load the left fsaverage label
-    #left_label_file_path = f'{FREESURFER_SUB}/fsaverage/label/lh.aparc.annot'
-    #left_label_data = nib.freesurfer.read_annot(left_label_file_path)
+    # left_label_file_path = f'{FREESURFER_SUB}/fsaverage/label/lh.aparc.annot'
+    # left_label_data = nib.freesurfer.read_annot(left_label_file_path)
 
     # Extract left label names and label indices
     left_label_names, left_label_indices = left_label_data[2], left_label_data[0]
 
-
     # Load the right fsaverage label
-    #right_label_file_path = f'{FREESURFER_SUB}/fsaverage/label/rh.aparc.annot'
-    #right_label_data = nib.freesurfer.read_annot(right_label_file_path)
+    # right_label_file_path = f'{FREESURFER_SUB}/fsaverage/label/rh.aparc.annot'
+    # right_label_data = nib.freesurfer.read_annot(right_label_file_path)
 
     # Extract right label names and label indices
     right_label_names, right_label_indices = right_label_data[2], right_label_data[0]
 
-    left_avg_thickness = np.zeros(len(left_label_names))
-    right_avg_thickness = np.zeros(len(right_label_names))
-
+    left_avg_thickness = np.zeros(len(cortical_roi_ids))
+    right_avg_thickness = np.zeros(len(cortical_roi_ids))
 
     left_thickness = nib.load(left_thickness_file).get_fdata()
     right_thickness = nib.load(right_thickness_file).get_fdata()
 
-
-    for i, r in enumerate(len(left_label_names)):
-
-        left_avg_thickness[i] = np.mean(left_thickness[left_label_indices == i])
+    for i, r in enumerate(cortical_roi_ids):
+        left_avg_thickness[i] = np.mean(left_thickness[left_label_indices == r])
         print(f"avg cortical thickness for roi {r} is {left_avg_thickness[i]}")
 
-    for i, r in enumerate(len(right_label_names)):
-
-        right_avg_thickness[i] = np.mean(right_thickness[right_label_indices == i])
+    for i, r in enumerate(cortical_roi_ids):
+        right_avg_thickness[i] = np.mean(right_thickness[right_label_indices == r])
         print(f"avg cortical thickness for roi {r} is {right_avg_thickness[i]}")
-
 
     return left_avg_thickness, right_avg_thickness
 
 
 def get_cortical_thickness(surf_thickness_file):
-
-    return nib.load(surf_thickness_file).get_fdata()
-
+    return nib.load(surf_thickness_file).get_fdata()[0, 0]
 
 
 def get_roi_vols(label_file, unique_labels):
-
     unique_labels = unique_labels.astype(int)
     # Load the NIfTI file
     nifti_img = nib.load(label_file)  # Replace with your NIfTI file path
 
     # Get the data array from the NIfTI image
     roi_data = nifti_img.get_fdata().astype(int)
-  
+
     # Get voxel dimensions (voxel size)
     voxel_size = np.prod(nifti_img.header.get_zooms())
 
@@ -90,49 +95,51 @@ def get_roi_vols(label_file, unique_labels):
         roi_volumes[i] = roi_volume
 
     # Print the ROI volumes (label, volume)
-    ''' for i, volume in enumerate(roi_volumes):
+    """ for i, volume in enumerate(roi_volumes):
         print(f"ROI {unique_labels[i]}: Volume = {volume} cubic units")
-    '''
+    """
     return roi_volumes
-    
 
 
 label_ids = np.unique(
     nib.load(
         "/deneb_disk/3T_vs_low_field/freesurfer_processed_subjects/fsaverage/mri/aparc+aseg.mgz"
-    ).get_fdata().astype(int)
+    )
+    .get_fdata()
+    .astype(int)
 )
 
 label_ids = np.unique(label_ids)
 
 
 # Load the left fsaverage surface
-fsaverage_surf_left = surface.load_surf_mesh(f'{FREESURFER_SUB}/fsaverage/surf/lh.white')
+fsaverage_surf_left = surface.load_surf_mesh(
+    f"{FREESURFER_SUB}/fsaverage/surf/lh.white"
+)
 
 # Load the right fsaverage surface
-fsaverage_surf_right = surface.load_surf_mesh(f'{FREESURFER_SUB}/fsaverage/surf/rh.white')
+fsaverage_surf_right = surface.load_surf_mesh(
+    f"{FREESURFER_SUB}/fsaverage/surf/rh.white"
+)
 
 # Load the left fsaverage label
-left_label_file_path = f'{FREESURFER_SUB}/fsaverage/label/lh.aparc.annot'
+left_label_file_path = f"{FREESURFER_SUB}/fsaverage/label/lh.aparc.annot"
 left_label_data = nib.freesurfer.read_annot(left_label_file_path)
 
 # Extract left label names and label indices
 left_label_names, left_label_indices = left_label_data[2], left_label_data[0]
 
-
 # Load the right fsaverage label
-right_label_file_path = f'{FREESURFER_SUB}/fsaverage/label/rh.aparc.annot'
+right_label_file_path = f"{FREESURFER_SUB}/fsaverage/label/rh.aparc.annot"
 right_label_data = nib.freesurfer.read_annot(right_label_file_path)
 
 # Extract right label names and label indices
 right_label_names, right_label_indices = right_label_data[2], right_label_data[0]
 
-
-
-
-
-
-
+# Get the unique label IDs
+cortical_label_ids = np.setdiff1d(
+    np.union1d(np.unique(left_label_indices), np.unique(right_label_indices)), (0, -1)
+)
 
 
 nsub = 5
@@ -140,6 +147,12 @@ param_list = ("1e-14", "2e-14")
 # param = param_list[0]
 
 roi_vols_lf = np.full((2, nsub, 2, len(label_ids)), np.nan)
+left_roi_thickness_lf = np.full((2, nsub, 2, len(cortical_label_ids)), np.nan)
+right_roi_thickness_lf = np.full((2, nsub, 2, len(cortical_label_ids)), np.nan)
+
+thickness_left_lf = np.full((2, nsub, 2, len(left_label_indices)), np.nan)
+thickness_right_lf = np.full((2, nsub, 2, len(right_label_indices)), np.nan)
+
 
 for sess, n, p in product((1, 2), range(1, nsub + 1), range(2)):
     param = param_list[p]
@@ -154,6 +167,10 @@ for sess, n, p in product((1, 2), range(1, nsub + 1), range(2)):
 
     sub_label_file = out_dir + "/mri/aparc+aseg.mgz"
     anat_img_file = out_dir + "/mri/T1.mgz"
+
+    sub_thickness_left_file = out_dir + "/surf/lh.thickness.fsaverage.mgh"
+    sub_thickness_right_file = out_dir + "/surf/rh.thickness.fsaverage.mgh"
+
     # Add the labels as an overlay
     plot_roi(
         roi_img=sub_label_file,
@@ -162,11 +179,35 @@ for sess, n, p in product((1, 2), range(1, nsub + 1), range(2)):
         alpha=0.5,
         draw_cross=False,
         colorbar=True,
-        output_file="subj" + str(n) + "_vol" + str(sess) + "_"+param+ "_LF_freesurfer.png",
+        output_file="subj"
+        + str(n)
+        + "_vol"
+        + str(sess)
+        + "_"
+        + param
+        + "_LF_freesurfer.png",
     )
 
     if os.path.isfile(sub_label_file):
         roi_vols_lf[sess - 1, n - 1, p, :] = get_roi_vols(sub_label_file, label_ids)
+        thickness_left_lf[sess - 1, n - 1, p, :] = get_cortical_thickness(
+            sub_thickness_left_file
+        )
+        thickness_right_lf[sess - 1, n - 1, p, :] = get_cortical_thickness(
+            sub_thickness_right_file
+        )
+
+        (
+            left_roi_thickness_lf[sess - 1, n - 1, p, :],
+            right_roi_thickness_lf[sess - 1, n - 1, p, :],
+        ) = get_roiwise_thickness(
+            sub_thickness_left_file,
+            sub_thickness_right_file,
+            left_label_data,
+            right_label_data,
+            cortical_label_ids,
+        )
+
     else:
         print(
             f"The following label file does not exist!! {sub_label_file} Skipping...:"
@@ -178,12 +219,23 @@ np.savez(
     nsub=nsub,
     param_list=param_list,
     label_ids=label_ids,
+    left_roi_thickness_lf=left_roi_thickness_lf,
+    right_roi_thickness_lf=right_roi_thickness_lf,
+    thickness_left_lf=thickness_left_lf,
+    thickness_right_lf=thickness_right_lf,
+    cortical_label_ids=cortical_label_ids,
 )
 
 print("Done! for LF")
 
 
 roi_vols_3t = np.full((2, nsub, len(label_ids)), np.nan)
+left_roi_thickness_3t = np.full((2, nsub, len(cortical_label_ids)), np.nan)
+right_roi_thickness_3t = np.full((2, nsub, len(cortical_label_ids)), np.nan)
+
+thickness_left_3t = np.full((2, nsub, len(left_label_indices)), np.nan)
+thickness_right_3t = np.full((2, nsub, len(right_label_indices)), np.nan)
+
 
 for sess, n in product((1, 2), range(1, nsub + 1)):
     out_dir = (
@@ -196,6 +248,9 @@ for sess, n in product((1, 2), range(1, nsub + 1)):
 
     sub_label_file = out_dir + "/mri/aparc+aseg.mgz"
     anat_img_file = out_dir + "/mri/T1.mgz"
+    sub_thickness_left_file = out_dir + "/surf/lh.thickness.fsaverage.mgh"
+    sub_thickness_right_file = out_dir + "/surf/rh.thickness.fsaverage.mgh"
+
     # Add the labels as an overlay
     plot_roi(
         roi_img=sub_label_file,
@@ -209,16 +264,39 @@ for sess, n in product((1, 2), range(1, nsub + 1)):
 
     if os.path.isfile(sub_label_file):
         roi_vols_3t[sess - 1, n - 1, :] = get_roi_vols(sub_label_file, label_ids)
+        thickness_left_3t[sess - 1, n - 1, :] = get_cortical_thickness(
+            sub_thickness_left_file
+        )
+
+        thickness_right_3t[sess - 1, n - 1, :] = get_cortical_thickness(
+            sub_thickness_right_file
+        )
+
+        (left_roi_thickness_3t[sess - 1, n - 1, :], right_roi_thickness_3t[sess - 1, n - 1, :]) = get_roiwise_thickness(
+            sub_thickness_left_file,
+            sub_thickness_right_file,
+            left_label_data,
+            right_label_data,
+            cortical_label_ids,
+        )
+
     else:
         print(
             f"The following label file does not exist!! {sub_label_file} Skipping...:"
         )
 
+
 np.savez(
     "freesurfer_3T.npz",
     roi_vols=roi_vols_3t,
     nsub=nsub,
-    param_list=param_list,
     label_ids=label_ids,
+    left_roi_thickness_3t=left_roi_thickness_3t,
+    right_roi_thickness_3t=right_roi_thickness_3t,
+    thickness_left_3t=thickness_left_3t,
+    thickness_right_3t=thickness_right_3t,
+    cortical_label_ids=cortical_label_ids,
 )
+
+
 print("Done! for 3T")
