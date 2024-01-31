3dbb991fe1b36710011161b01a108f07e696a5e0
Anand A. Joshi <ajoshi@usc.edu>
2024-01-31 12:36:54 -0800

    update

--- low_field_high_field_mprage_comparison/diffusion_comparison/main_map2atlas.py
+++ low_field_high_field_mprage_comparison/diffusion_comparison/main_map2atlas.py
@@ -1,11 +1,15 @@
-#AUM
-#Shree Ganeshaya Namaha
+# AUM
+# Shree Ganeshaya Namaha
 import os
+from itertools import product
 
-
-for subno in range(1,6):
-
-    cmd = f"~/Software/BrainSuite23a/svreg/bin/svreg_apply_map.sh /deneb_disk/3T_vs_low_field/FA_MD_for_common_space/MPRAGE_BrainSuite_4bfc/V1_LF_mprage_vol1_recon_GNL/T1.svreg.inv.map.nii.gz /deneb_disk/3T_vs_low_field/FA_MD_for_common_space/MD/V1_LF_SER_MD_vol1_wm.nii.gz /deneb_disk/3T_vs_low_field/FA_MD_for_common_space/MD/V1_LF_SER_MD_vol1_wm.atlas.nii.gz /home/ajoshi/Software/BrainSuite23a/svreg/BrainSuiteAtlas1/mri.bfc.nii.gz"
+for subno, vol in product(range(1, 6), range(1, 3)):
+    # Warp MD to atlas
+    cmd = f"/home/ajoshi/Software/BrainSuite23a/svreg_23a_build2505_linux/bin/svreg_apply_map.sh /deneb_disk/3T_vs_low_field/FA_MD_for_common_space/MPRAGE_BrainSuite_4bfc/V{subno}_LF_mprage_vol1_recon_GNL/T1.svreg.inv.map.nii.gz /deneb_disk/3T_vs_low_field/FA_MD_for_common_space/new_FA_MD/MD/V{subno}_LF_SER_MD_vol{vol}_T1_GNL.nii.gz /deneb_disk/3T_vs_low_field/FA_MD_for_common_space/new_FA_MD/MD_warped/V{subno}_LF_SER_MD_vol{vol}_T1_GNL.atlas.nii.gz /home/ajoshi/Software/BrainSuite23a/svreg/BrainSuiteAtlas1/mri.bfc.nii.gz 0"
+    print(cmd)
     os.system(cmd)
 
-    
+    # Warp FA to atlas
+    cmd = f"/home/ajoshi/Software/BrainSuite23a/svreg_23a_build2505_linux/bin/svreg_apply_map.sh /deneb_disk/3T_vs_low_field/FA_MD_for_common_space/MPRAGE_BrainSuite_4bfc/V{subno}_LF_mprage_vol1_recon_GNL/T1.svreg.inv.map.nii.gz /deneb_disk/3T_vs_low_field/FA_MD_for_common_space/new_FA_MD/FA/V{subno}_LF_SER_FA_vol{vol}_T1_GNL.nii.gz /deneb_disk/3T_vs_low_field/FA_MD_for_common_space/new_FA_MD/FA_warped/V{subno}_LF_SER_FA_vol{vol}_T1_GNL.atlas.nii.gz /home/ajoshi/Software/BrainSuite23a/svreg/BrainSuiteAtlas1/mri.bfc.nii.gz 0"
+    print(cmd)
+    os.system(cmd)
