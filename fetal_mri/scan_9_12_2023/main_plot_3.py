c51856a52bb3f91ea6d55d22d4cfc8a066f6f035
Anand A. Joshi <ajoshi@usc.edu>
2024-06-11 00:47:24 -0700

    update

---
+++ fetal_mri/scan_9_12_2023/main_plot_3.py
@@ -0,0 +1,32 @@
+import nibabel as nib
+import numpy as np
+import imageio
+from nilearn.plotting import plot_anat
+
+# Load the NIfTI image
+nifti_file = "/deneb_disk/disc_mri/scan_9_12_2023/outsvr/svr_te140_numstacks_12_iter_0_aligned.nii.gz"  # Replace with your input NIfTI file
+
+atlas_file = (
+    "/deneb_disk/disc_mri/fetal_atlas/CRL_FetalBrainAtlas_2017v3/STA37exp.nii.gz"
+)
+nifti_file_masked = "/deneb_disk/disc_mri/scan_9_12_2023/outsvr/svr_te140_numstacks_12_iter_0_aligned_masked.nii.gz"
+
+# Load the NIfTI image, read atlas image, mask the image with the atlas
+img = nib.load(nifti_file)
+atlas = nib.load(atlas_file)
+data = img.get_fdata()
+atlas_data = atlas.get_fdata()
+data[atlas_data == 0] = 0
+img = nib.Nifti1Image(data, img.affine, img.header)
+nib.save(img, nifti_file_masked)
+
+
+plot_anat(
+    nifti_file_masked,
+    display_mode="ortho",
+    vmin=0,
+    vmax=1200,
+    cut_coords=(0, 0, 0),
+    output_file="svr_te140_numstacks_12_iter_0_aligned_masked.png",
+    draw_cross=False,
+)
