3965d15b90f5850566e5c049cb5ec51d5c146720
Anand A. Joshi <ajoshi@usc.edu>
2026-02-21 21:34:46 -0800

    Add new scripts for DICOM to NIfTI conversion, image padding, and SVR reconstruction

---
+++ heart_svr/scan_12_17_2025/vol1406_20251217/main_pad_imgage.py
@@ -0,0 +1,62 @@
+import nibabel as nib
+from nilearn.image import new_img_like
+import numpy as np
+
+import nibabel as nib
+import numpy as np
+import nibabel as nib
+import numpy as np
+
+
+def pad_nifti(input_file, output_file, pad_width):
+    # Load the NIfTI file
+    nifti_img = nib.load(input_file)
+    data = nifti_img.get_fdata()
+    affine = nifti_img.affine
+
+    # Pad the data
+    padded_data = np.pad(data, pad_width=((pad_width, pad_width),
+                                          (pad_width, pad_width),
+                                          (pad_width, pad_width)),
+                         mode='constant', constant_values=0)
+
+    # Calculate voxel sizes
+    voxel_sizes = np.sqrt((affine[:3, :3] ** 2).sum(axis=0))
+
+    # Adjust the affine matrix for the new origin
+    new_affine = affine.copy()
+    
+    translation_adjustment = np.eye(4)
+    translation_adjustment[:3, 3] = -pad_width #* voxel_sizes
+    new_affine = new_affine @ translation_adjustment
+
+    # Create a new NIfTI image
+    padded_img = nib.Nifti1Image(padded_data, affine=new_affine)
+
+    # Save the padded NIfTI file
+    nib.save(padded_img, output_file)
+
+
+
+
+# Define the path to the template and mask images
+template = "/deneb_disk/disc_mri/for_Ye_12_11_2024_4subjects/nifti_files/vol1052_20241204_standard_view/phase_13_rot/p70_cardiac_svr_sweep_2_res_15.nii.gz"
+mask = "/deneb_disk/disc_mri/for_Ye_12_11_2024_4subjects/nifti_files/vol1052_20241204_standard_view/phase_13_rot/p70_cardiac_svr_sweep_2_res_15.mask.nii.gz"
+
+padded_template_filename = "/deneb_disk/disc_mri/for_Ye_12_11_2024_4subjects/vol1052_20241204_standard_view_template/p70_cardiac_svr_sweep_2_res_15.pad.nii.gz"
+padded_mask_filename = "/deneb_disk/disc_mri/for_Ye_12_11_2024_4subjects/vol1052_20241204_standard_view_template/p70_cardiac_svr_sweep_2_res_15.pad.mask.nii.gz"
+
+pad_width = 20
+
+# Example usage
+input_file = template
+output_file = padded_template_filename
+
+pad_nifti(input_file, output_file, pad_width)
+
+# Example usage
+input_file = mask
+output_file = padded_mask_filename
+
+pad_nifti(input_file, output_file, pad_width)
+
---
+++ heart_svr/scan_12_17_2025/vol1406_20251217/main_svr_singularity.py
@@ -0,0 +1,96 @@
+"""
+SVR reconstructions for different combinations of stacks
+"""
+
+import os
+import glob
+from itertools import product
+
+
+def svr(subdir, template, mask, outsvr_dir, outsvr, res=1.0, slice_thickness=6.0):
+
+    stacks_all = glob.glob(subdir + "/*.nii.gz")
+
+    stacks = ""
+    num_stacks = len(stacks_all)
+    
+    #print("*********************************")
+    #print(num_stacks)
+    #print("*********************************")
+
+    if num_stacks != 3:
+        print("Number of stacks is not 4 Check!!")
+        print(stacks_all)
+        return
+
+    str_th = ""
+
+    for j in range(num_stacks):
+        stacks += " " + stacks_all[j]
+        str_th += " " + str(slice_thickness)
+
+    cmd = (
+        f"cd {outsvr_dir}; mirtk reconstruct "
+        + outsvr
+        + " "
+        + str(num_stacks)
+        + stacks
+        + " --resolution "
+        + str(res)
+    )
+
+    cmd += " --thickness " + str_th + " --template " + template + " --mask " + mask
+
+    #print(cmd)
+    # os.system(cmd)
+    docker_cmd = f"singularity run --bind /project/ajoshi_27 /scratch1/ajoshi/svrtk_latest.sif /bin/bash -lic \\\"{cmd}\\\" "
+    #print(docker_cmd)
+    full_cmd = 'sbatch '+ 'mycmd.sh "' + docker_cmd +"\""
+    print(full_cmd)
+    os.system(full_cmd)
+
+
+if __name__ == "__main__":
+
+
+    #scans_dir_top = "/project/ajoshi_27/disc_mri/heart_svr_acquisition_10_26_2024/nifti_files"
+    #expmt_dir_all = glob.glob(scans_dir_top + "/vol0929*")
+
+    scans_dir_top =  '/project/ajoshi_27/disc_mri/for_Ye_12_11_2024_4subjects/nifti_files'
+    expmt_dir_all = glob.glob(scans_dir_top + "/vol1052_20241204_standard_view")
+
+    for phase, expmt_dir in product(range(25), expmt_dir_all):
+
+        res = 1.0
+        subdir = expmt_dir + f"/phase_{phase+1:02}_rot"
+        template = "/project/ajoshi_27/disc_mri/for_Ye_12_11_2024_4subjects/vol1052_20241204_standard_view_template/p70_cardiac_svr_sweep_2_res_15.pad.nii.gz"
+        mask = "/project/ajoshi_27/disc_mri/for_Ye_12_11_2024_4subjects/vol1052_20241204_standard_view_template/p70_cardiac_svr_sweep_2_res_15.pad.dilated.mask.nii.gz"
+
+        outsvr = (
+            f"svr_heart_"
+            + expmt_dir.split("/")[-1]
+            + f"_phase_{phase+1:02}_res_{res:.1f}.nii.gz"
+        )
+
+        expdir_prefix = expmt_dir.split("/")[-1]
+        outsvr_dir = f"/project/ajoshi_27/disc_mri/for_Ye_12_11_2024_4subjects/outsvr_pad/{expdir_prefix}/" + f"phase_{phase+1:02}_allstacks"
+
+        os.makedirs(outsvr_dir)
+
+        thickness = 6.0
+
+        # convert thickness to float
+        thickness = float(thickness)
+
+        svr(
+            subdir,
+            template,
+            mask,
+            outsvr_dir,
+            outsvr,
+            res=res,
+            slice_thickness=thickness,
+        )
+
+
+# End of main_svr.py
