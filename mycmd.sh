60cf97c839b27cafa5917b30036c73911168fc93
Anand Joshi <ajoshi@discovery2.hpc.usc.edu>
2026-02-21 17:26:21 -0800

    used epyc-64 partition

--- mycmd.sh
+++ mycmd.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 #SBATCH --account=ajoshi_27
-#SBATCH --partition=main
+#SBATCH --partition=epyc-64
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=8
 #SBATCH --mem=64G
-#SBATCH --time=1:00:00
+#SBATCH --time=0:50:00
 
 #module purge
 #singularity exec --bind $PWD,/project2/ajoshi_27 /scratch1/ajoshi/svrtk_latest.sif Rscript script.R
